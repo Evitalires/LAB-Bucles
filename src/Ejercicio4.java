a5385370b93f89f5e6c94208198015482c5c7ba3
Evitalires <evitalires@gmail.com>
2025-12-01 14:43:55 -0500

    Ejercicio 4

---
+++ src/Ejercicio4.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    public static void CajeroAutomatico() {
+        double saldo = 0;
+        int opcionUsuario;
+        int cantidad;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\n --- Bienvenido a BancGeneration ---");
+        do {
+            cantidad = 0;
+            opcionUsuario = 0;
+            System.out.println("\n -- Menu de Eleccion --");
+            System.out.println("\n 1. Consultar Saldo");
+            System.out.println("\n 2. Retirar Efectivo");
+            System.out.println("\n 3. Depositar Efectivo");
+            System.out.println("\n 4. Finalizar sesion.");
+
+            System.out.println("\n -- Escriba el numero de la opcion: --");
+            opcionUsuario = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcionUsuario) {
+                case 1:
+                    System.out.println("Su saldo actual es: " + saldo);
+                    break;
+                case 2:
+                    System.out.println("Cuanto efectivo va retirar? ");
+                    double valorRetiro = scanner.nextDouble();
+                    saldo -= valorRetiro;
+                    System.out.println("Su nuevo saldo es: $" + saldo);
+                    break;
+                case 3:
+                    System.out.println("Cuanto efectivo va a depositar? ");
+                    double valorDeposito = scanner.nextDouble();
+                    saldo += valorDeposito;
+                    System.out.println("Su nuevo saldo es: $" + saldo);
+                    break;
+                default:
+                    System.out.println("-- ❌ Opcion no disponible ❌ --");
+            }
+        } while (opcionUsuario != 4);
+        scanner.close();
+        System.out.println("Gracias por usar nuestros servicios!");
+    }
+}
